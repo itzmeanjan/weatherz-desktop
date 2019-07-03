805b90e979c6956efe46fa116ddcb2dd5a1f258d
Anjan Roy <anjanroy@yandex.com>
2019-07-03 13:16:41 +0530

    added world map generator

---
+++ map/README.md
@@ -0,0 +1,4 @@
+## mapRenderer
+A collection of Python Scripts to render map of World and Countries, to be used in [weatherz-desktop](https://github.com/itzmeanjan/weatherz-desktop)
+
+**More coming soon ...**
---
+++ map/build.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from __future__ import annotations
+from os.path import abspath, join, dirname
+
+try:
+    import mapnik
+except ImportError as e:
+    print('[!]Module Unavailable : {}'.format(str(e)))
+    exit(1)
+
+
+def renderIt(style_sheet: str = abspath(join(dirname(__file__), 'world.xml'))):
+    target = False
+    try:
+        myMap = mapnik.Map(6400, 3600)
+        mapnik.load_map(myMap, style_sheet)
+        myMap.zoom_all()
+        mapnik.render_to_file(myMap, 'world.jpeg', 'jpeg100')
+        target = True
+    except Exception:
+        target = False
+    finally:
+        return target
+
+
+if __name__ == '__main__':
+    try:
+        print('Success' if renderIt() else 'Failure')
+    except KeyboardInterrupt:
+        print('\n[!]Terminated')
+    finally:
+        exit(0)
