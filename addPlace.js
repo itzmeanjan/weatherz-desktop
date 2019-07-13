5092b21a65fd33f8a8f7370c58a5b525210dcd18
Anjan Roy <anjanroy@yandex.com>
2019-07-13 23:21:09 +0530

    added some docs

--- addPlace.js
+++ addPlace.js
@@ -2,6 +2,14 @@
 
 const fs = require('fs');
 
+// extracts all existing place records from target file
+// these are the places for which this app will fetch weather data
+// from `Yr.no`
+// if there's no place record yet, listen for error in returned Promise,
+// that's what will be populated
+// otherwise an object of `./model/place.AllPlaceEntries` to be returned via Promise
+// data read from target_file will be objectified and enclosed in aforementioned class
+
 function extractIt(target_file = './data/addedPlaces.json') {
     return new Promise((resolve, reject) => {
         try {
@@ -23,6 +31,9 @@ function extractIt(target_file = './data/addedPlaces.json') {
     });
 }
 
+// writes JSON stringified data into target file
+// JSON data, to be written, is passed in well formatted form
+
 function writeIt(data, target_file = './data/addedPlaces.json') {
     return new Promise((resolve, reject) => {
         fs.writeFile(target_file, data, (err) => {
@@ -34,22 +45,44 @@ function writeIt(data, target_file = './data/addedPlaces.json') {
     });
 }
 
+// adds requested place to record, which is kept in ./data/addedPlaces.json
+// placeData will be of following form
+// {
+//      name: 'X',
+//      url: 'x'   
+// }
+// which will be serialized into `./model/place._IndividualPlaceEntry` object
+// and placed in `.model/place.AllPlaceEntries` object, which holds all places
+// for which this app will fetch data from `Yr.no`
+
 function addIt(placeData, target_file = './data/addedPlaces.json') {
     return new Promise(
-        (reject, resolve) => extractIt().then(
+        (reject, resolve) => extractIt(target_file).then(
             (value) =>
                 writeIt(JSON.stringify(value.appendAnother(placeData).toJSON(),
-                    null, 4))
+                    null, 4), target_file)
                     .then((val) => resolve(val), (err) => reject(err)),
             (err) =>
                 writeIt(JSON.stringify(
                     require('./model/place').instance
                         .appendAnother(placeData)
                         .toJSON(),
-                    null, 4))
+                    null, 4), target_file)
                     .then((val) => resolve(val), (err) => reject(err))
         ));
 }
 
+// making it available to use from outside
+
+module.exports = {
+    addIt: addIt, // to add new record
+    extractIt: extractIt // to extract existing record(s)
+};
+
+
+// following section was used to check whether it works as expected or not
+
+/*
 addIt({ name: 'Bolpur', url: 'http://yr.no/Bolpur.xml' })
     .then((val) => console.log(val), (err) => console.log(err));
+*/
--- model/place.js
+++ model/place.js
@@ -1,10 +1,18 @@
 'use strict';
 
+// holds record of a certain place, which is present in ./data/addedPlaces.json
+// will fetch weather data of this place
+// will also be used to display already added place record
+//
+// keeps place name and corresponding url, from which weather data to be fetched
+
 class _IndividualPlaceEntry {
     constructor(name, url) {
         this.name = name;
         this.url = url;
     }
+
+    // this one will help in creating an object of this class from JSON
 
     static fromJSON(jsonObject) {
         let _individualPlaceEntry = new _IndividualPlaceEntry(null, null);
@@ -13,24 +21,41 @@ class _IndividualPlaceEntry {
         return _individualPlaceEntry;
     }
 
-    toJSON() {
-        return {
-            name: this.name,
-            url: this.url
-        };
-    }
+    // will be useful in converting place record to JSON string,
+    // which is to be eventually stored in ./data/addedPlaces.json
+
+    toJSON() { return { name: this.name, url: this.url }; }
 }
 
+// keeps a list of all places for which user is intertested in getting
+// weather data
+//
+// each element of allPlaces will be an object of `_IndividualPlaceEntry` class
+
 class AllPlaceEntries {
-    constructor(allPlaces) {
-        this.allPlaces = allPlaces;
-    }
+    constructor(allPlaces) { this.allPlaces = allPlaces; }
+
+    // takes a JS array where each element is of { name: 'X', url: 'x' } form
+    // and returns an instance of this class, holding all places
 
     static fromJSON(jsonObject) {
         let allPlaceEntries = new AllPlaceEntries([]);
-        allPlaceEntries.allPlaces = jsonObject.map((elem) => _IndividualPlaceEntry.fromJSON(elem));
+        allPlaceEntries.allPlaces =
+            jsonObject.map((elem) => _IndividualPlaceEntry.fromJSON(elem));
         return allPlaceEntries;
     }
+
+    // this one will be helpful in appending a new place record
+    // to `allPlaces`
+    // returns `this`, so that method calling can be changed
+    //
+    // * seems helpful in my view
+    //
+    // well there may be a situation, when someone creates
+    // an instance of this class with `null` set as allPlaces
+    // then this method will allocate a new JS array to `allPlaces`
+    // with an object of `_IndividualPlaceEntry`, which will hold record
+    // this place
 
     appendAnother(jsonObject) {
         if (this.allPlaces !== undefined && this.allPlaces !== null)
@@ -40,19 +65,17 @@ class AllPlaceEntries {
         return this;
     }
 
+    // helpful while exporting this object to JSON string
+    // and storing it in a certain file
+
     toJSON() {
-        return (this.allPlaces !== undefined && this.allPlaces !== null) ?
-            {
-                places: this.allPlaces.map((elem) => elem.toJSON())
-            }
-            :
-            {
-                places: []
-            };
+        return (this.allPlaces !== undefined && this.allPlaces !== null)
+            ? { places: this.allPlaces.map((elem) => elem.toJSON()) }
+            : { places: [] };
     }
 }
 
 module.exports = {
-    AllPlaceEntries: AllPlaceEntries,
-    instance: new AllPlaceEntries([])
+    AllPlaceEntries: AllPlaceEntries, // use it when you're interested in invoking static method `fromJSON`
+    instance: new AllPlaceEntries([]) // and this one, when in need of just appending one/ more record(s), and finally exporting them to JSON string
 };
